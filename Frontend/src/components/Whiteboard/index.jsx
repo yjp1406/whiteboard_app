a5f1d9a5429cc032da5b725d2e9237b5e9d02d39
Yagnesh Patel <yagneshjp1406@gmail.com>
2025-07-05 20:51:14 +0530

    clear canvas, change color and eraser functionality done

--- Frontend/src/components/Whiteboard/index.jsx
+++ Frontend/src/components/Whiteboard/index.jsx
@@ -1,126 +1,164 @@
-import { useState, useEffect , useLayoutEffect, use} from "react";
+import { useState, useEffect, useLayoutEffect, use } from "react";
 import rough from "roughjs";
 
 const roughGenerator = rough.generator();
 
-const Whiteboard = ({ canvasRef, ctxRef, elements, setElements , tool}) => {
+const Whiteboard = ({
+  canvasRef,
+  ctxRef,
+  elements,
+  setElements,
+  tool,
+  color,
+}) => {
   const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth*2;
-    canvas.height = window.innerHeight/2;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight / 1.5;
     const ctx = canvas.getContext("2d");
-    ctxRef.current = ctx;   
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+
+    ctxRef.current = ctx;
   }, []);
+
+  useEffect(() => {
+    ctxRef.current.strokeStyle = color;
+  }, [color]);
 
   useLayoutEffect(() => {
     const roughCanvas = rough.canvas(canvasRef.current);
 
-    if(elements.length>0){
-        ctxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    if (elements.length > 0) {
+      ctxRef.current.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
     }
     elements.forEach((element) => {
-        if(element.type === "pencil") {
-        roughCanvas.linearPath(element.path);
-        }else if(element.type === "line") {
-        roughCanvas.draw(roughGenerator.line(element.offsetX, element.offsetY, element.width, element.height));
-        } else if(element.type === "rect") {
-        roughCanvas.draw(roughGenerator.rectangle(element.offsetX, element.offsetY, element.width, element.height));
-        }
+      if (element.type === "pencil" || element.type === "eraser") {
+        roughCanvas.linearPath(element.path, {
+          stroke: element.stroke,
+          strokeWidth: element.strokeWidth || 5,
+          roughness: 0,
+        });
+      } else if (element.type === "line") {
+        roughCanvas.draw(
+          roughGenerator.line(
+            element.offsetX,
+            element.offsetY,
+            element.width,
+            element.height,
+            { stroke: element.stroke, strokeWidth: 5, roughness: 0 }
+          )
+        );
+      } else if (element.type === "rect") {
+        roughCanvas.draw(
+          roughGenerator.rectangle(
+            element.offsetX,
+            element.offsetY,
+            element.width,
+            element.height,
+            { stroke: element.stroke, strokeWidth: 5, roughness: 0 }
+          )
+        );
+      }
     });
-  },[elements]);
+  }, [elements]);
 
   const handleMouseMove = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
 
     if (isDrawing) {
-        
-        if(tool === "pencil") {
-            const {path} = elements[elements.length - 1];
-            const newPath = [...path, [offsetX, offsetY]];
-            setElements((prevElements) => 
-                prevElements.map((ele,index) => {
-                    if (index === elements.length - 1) {
-                        return {
-                            ...ele,
-                            path: newPath,
-                        }
-                    }
-                    return ele;
-                })
-            );
-        }else if(tool === "line") {
-            setElements((prevElements) => 
-                prevElements.map((ele,index) => {
-                    if (index === elements.length - 1) {
-                        return {
-                            ...ele,
-                            width:offsetX,
-                            height:offsetY,
-                    
-                            // path: newPath,
-                        }
-                    }
-                    return ele;
-                })
-            );
-        } else if(tool === "rect") {
-            setElements((prevElements) => 
-                prevElements.map((ele,index) => {
-                    if (index === elements.length - 1) {
-                        return {
-                            ...ele,
-                            width:offsetX - ele.offsetX,
-                            height:offsetY - ele.offsetY,
-                        }
-                    }
-                    return ele;
-                })
-            );
-        }
-
+      if (tool === "pencil" || tool === "eraser") {
+        const { path } = elements[elements.length - 1];
+        const newPath = [...path, [offsetX, offsetY]];
+        setElements((prevElements) =>
+          prevElements.map((ele, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...ele,
+                path: newPath,
+              };
+            }
+            return ele;
+          })
+        );
+      } else if (tool === "line") {
+        setElements((prevElements) =>
+          prevElements.map((ele, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...ele,
+                width: offsetX,
+                height: offsetY,
+              };
+            }
+            return ele;
+          })
+        );
+      } else if (tool === "rect") {
+        setElements((prevElements) =>
+          prevElements.map((ele, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...ele,
+                width: offsetX - ele.offsetX,
+                height: offsetY - ele.offsetY,
+              };
+            }
+            return ele;
+          })
+        );
+      }
     }
   };
   const handleMouseDown = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
 
-    if(tool === "pencil") {
-        setElements((prevElements) => [
-          ...prevElements,
-          {
-            type: "pencil", // You can change this based on the selected tool
-            offsetX,
-            offsetY,
-            path: [[offsetX, offsetY]], // Store the path as an array of points
-            stroke: "black", // You can change this to use a color state
-          },
-        ]);
-    }   else if(tool === "line") {
-        setElements((prevElements) => [
-          ...prevElements,
-          {
-            type: "line", // You can change this based on the selected tool
-            offsetX,
-            offsetY,
-            width: offsetX, // Initial width and height will be updated on mouse move
-            height: offsetY,
-            stroke: "black", // You can change this to use a color state    
-        },]);
+    if (tool === "pencil" || tool === "eraser") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "pencil", // You can change this based on the selected tool
+          offsetX,
+          offsetY,
+          path: [[offsetX, offsetY]], // Store the path as an array of points
+          stroke: tool === "eraser" ? "white" : color,
+          strokeWidth: tool === "eraser" ? 20 : 5 // You can change this to use a color state
+        },
+      ]);
+    } else if (tool === "line") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "line", // You can change this based on the selected tool
+          offsetX,
+          offsetY,
+          width: offsetX, // Initial width and height will be updated on mouse move
+          height: offsetY,
+          stroke: color, // You can change this to use a color state
+        },
+      ]);
+    } else if (tool === "rect") {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: "rect", // You can change this based on the selected tool
+          offsetX,
+          offsetY,
+          width: 0, // Initial width and height will be updated on mouse move
+          height: 0,
+          stroke: color, // You can change this to use a color state
+        },
+      ]);
     }
-    else if(tool === "rect") {
-        setElements((prevElements) => [
-          ...prevElements,
-          {
-            type: "rect", // You can change this based on the selected tool
-            offsetX,
-            offsetY,
-            width: 0, // Initial width and height will be updated on mouse move
-            height: 0,
-            stroke: "black", // You can change this to use a color state    
-        },]);
-    }
-
 
     setIsDrawing(true);
   };
@@ -131,14 +169,13 @@ const Whiteboard = ({ canvasRef, ctxRef, elements, setElements , tool}) => {
 
   return (
     <div
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={() => setIsDrawing(false)}
-        className="border border-dark border-3 h-100 w-100 overflow-hidden">
-            <canvas
-              ref={canvasRef}
-            />
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={() => setIsDrawing(false)}
+      className="border border-dark border-3 h-100 w-100 overflow-hidden"
+    >
+      <canvas ref={canvasRef} />
     </div>
   );
 };
--- Frontend/src/pages/RoomPage/index.jsx
+++ Frontend/src/pages/RoomPage/index.jsx
@@ -3,53 +3,111 @@ import "./index.css";
 import { useRef, useState } from "react";
 
 const RoomPage = () => {
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
 
-    const canvasRef = useRef(null);
-    const ctxRef = useRef(null);
+  const [tool, setTool] = useState("pencil");
+  const [color, setColor] = useState("black");
+  const [elements, setElements] = useState([]);
 
-    const [tool,setTool] = useState("pencil");
-    const [color,setColor] = useState("black");
-    const [elements, setElements] = useState([]);
+  const handleClearCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = ctxRef.current;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setElements([]);
+  };
 
   return (
-    <div className='row'>
-      <h1 className="text-center py-5">White Board {" "}
-        <span className="text-primary">[Users Online : 0]</span>
+    <div className="row">
+      <h1 className="text-center py-5">
+        White Board <span className="text-primary">[Users Online : 0]</span>
       </h1>
-        <div className="col-md-10 mx-auto gap-3 px-5 mb-5 d-flex align-items-center justify-content-center">
-            <div className="d-flex col-md-2 justify-content-center gap-2">
-                <div className="d-flex gap-1 align-items-center">
-                    <label htmlFor="pencil" >Pencil</label>
-                    <input type="radio" id="pencil" name="tool" value="pencil" checked={tool=="pencil"} className="" onChange={(e) => setTool(e.target.value)} />
-                </div>
-                <div className="d-flex gap-1 align-items-center">
-                    <label htmlFor="line" >Line</label>
-                    <input type="radio" id="line" name="tool" value="line" checked={tool=="line"} className="" onChange={(e) => setTool(e.target.value)} /> 
-                    </div>
-                <div className="d-flex gap-1 align-items-center">
-                    <label htmlFor="rect" >Reactangle</label>
-                    <input type="radio" id="rect" name="tool" value="rect" checked={tool=="rect"} className="" onChange={(e) => setTool(e.target.value)} />
-                </div>
-            </div>
-            <div className="col-md-3 mx-auto">
-                <div className="d-flex align-items-center justify-content-center">
-                    <label htmlFor="color">Select Color:</label>
-                    <input type="color" id="color" className="ms-3" value={color} onChange={(e) => setColor(e.target.value)}/>
-                </div>
-            </div>
-            <div className="col-md-3 d-flex gap-2">
-                <button className="btn btn-primary mt-1">Undo</button>
-                <button className="btn btn-outline-primary mt-1">Redo</button>
-            </div>
-            <div className="col-md-2">
-                <button className="btn btn-danger">Clear Board</button>
-            </div>
+      <div className="col-md-10 mx-auto gap-3 px-5 mb-5 d-flex align-items-center justify-content-center">
+        <div className="d-flex col-md-2 justify-content-center gap-2">
+          <div className="d-flex gap-1 align-items-center">
+            <label htmlFor="pencil">Pencil</label>
+            <input
+              type="radio"
+              id="pencil"
+              name="tool"
+              value="pencil"
+              checked={tool == "pencil"}
+              className=""
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-1 align-items-center">
+            <label htmlFor="line">Line</label>
+            <input
+              type="radio"
+              id="line"
+              name="tool"
+              value="line"
+              checked={tool == "line"}
+              className=""
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-1 align-items-center">
+            <label htmlFor="rect">Reactangle</label>
+            <input
+              type="radio"
+              id="rect"
+              name="tool"
+              value="rect"
+              checked={tool == "rect"}
+              className=""
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div className="d-flex gap-1 align-items-center">
+            <label htmlFor="eraser" title="Eraser">
+              <i className="bi bi-eraser-fill fs-5"></i>
+            </label>
+            <input
+              type="radio"
+              id="eraser"
+              name="tool"
+              value="eraser"
+              checked={tool === "eraser"}
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="col-md-10 mx-auto h-100">
-            <Whiteboard canvasRef={canvasRef} ctxRef={ctxRef} elements={elements} setElements={setElements} tool={tool}/>
+        <div className="col-md-3 mx-auto">
+          <div className="d-flex align-items-center justify-content-center">
+            <label htmlFor="color">Select Color:</label>
+            <input
+              type="color"
+              id="color"
+              className="ms-3"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </div>
         </div>
+        <div className="col-md-3 d-flex gap-2">
+          <button className="btn btn-primary mt-1">Undo</button>
+          <button className="btn btn-outline-primary mt-1">Redo</button>
+        </div>
+        <div className="col-md-2">
+          <button className="btn btn-danger" onClick={handleClearCanvas}>
+            Clear Canvas
+          </button>
+        </div>
+      </div>
+      <div className="col-md-10 mx-auto h-100">
+        <Whiteboard
+          canvasRef={canvasRef}
+          ctxRef={ctxRef}
+          elements={elements}
+          setElements={setElements}
+          tool={tool}
+          color={color}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default RoomPage;
